811e309c5d83f76fd5c7a940e8d618e77447abf7
moabalv <moab.alves.silva@ccc.ufcg.edu.br>
2026-01-25 23:23:53 -0300

    fix: open requests

--- Projeto_1/Contrack/Project/src/main/java/com/Contrack/controller/DocumentoController.java
+++ Projeto_1/Contrack/Project/src/main/java/com/Contrack/controller/DocumentoController.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:5173")
 @RequestMapping(value = "/documentos", produces = MediaType.APPLICATION_JSON_VALUE)
 public class DocumentoController {
 
