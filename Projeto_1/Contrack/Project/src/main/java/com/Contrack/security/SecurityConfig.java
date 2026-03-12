6ffc089cf477b3fbd96d0b27d28c5a8718c6597a
moabalv <moab.alves.silva@ccc.ufcg.edu.br>
2026-03-12 15:20:11 -0300

    dev: allow ec2

--- Projeto_1/Contrack/Project/src/main/java/com/Contrack/security/SecurityConfig.java
+++ Projeto_1/Contrack/Project/src/main/java/com/Contrack/security/SecurityConfig.java
@@ -78,7 +78,8 @@ public class SecurityConfig {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOriginPatterns(List.of(
                 "http://localhost:*",
-                "http://127.0.0.1:*"
+                "http://127.0.0.1:*",
+                "http://3.150.249.219:*"
         ));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
