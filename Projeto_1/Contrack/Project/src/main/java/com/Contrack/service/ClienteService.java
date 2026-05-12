489a58a762a0c56c8c04eb3d7531f110074ac8e3
iorranlira <iorranlira@gmail.com>
2026-05-12 18:13:11 -0300

    feat(service): atualiza interfaces para suportar métodos com UserDetails

--- Projeto_1/Contrack/Project/src/main/java/com/Contrack/service/ClienteService.java
+++ Projeto_1/Contrack/Project/src/main/java/com/Contrack/service/ClienteService.java
@@ -2,10 +2,13 @@ package com.Contrack.service;
 
 import com.Contrack.dto.ClientePostPutRequestDTO;
 import com.Contrack.dto.ClienteResponseDTO;
+
 import java.util.List;
 
+import org.springframework.security.core.userdetails.UserDetails;
+
 public interface ClienteService {
-    List<ClienteResponseDTO> listarClientes(String ordenarPor);
+    List<ClienteResponseDTO> listarClientes(String ordenarPor, UserDetails userDetails);
     ClienteResponseDTO listarClienteId(Long id);
     ClienteResponseDTO criarCliente(ClientePostPutRequestDTO clienteDTO);
     List<ClienteResponseDTO> listarClientesPorNome(String nome);
--- Projeto_1/Contrack/Project/src/main/java/com/Contrack/service/DocumentoService.java
+++ Projeto_1/Contrack/Project/src/main/java/com/Contrack/service/DocumentoService.java
@@ -5,11 +5,13 @@ import com.Contrack.dto.DocumentoResponseDTO;
 
 import java.util.List;
 
+import org.springframework.security.core.userdetails.UserDetails;
+
 public interface DocumentoService {
-    List<DocumentoResponseDTO> listarDocumentos(String ordenarPor);
-    DocumentoResponseDTO buscarDocumento(Long id);
+    List<DocumentoResponseDTO> listarDocumentos(String ordenarPor, UserDetails userDetails);
+    DocumentoResponseDTO buscarDocumento(Long id, UserDetails userDetails);
     DocumentoResponseDTO criarDocumento(DocumentoRequestDTO documentoRequestDTO);
-    DocumentoResponseDTO atualizaDocumento (DocumentoRequestDTO documentoRequestDTO);
-    List<DocumentoResponseDTO> getDocumentoByClienteId(Long clienteId);
+    DocumentoResponseDTO atualizaDocumento (DocumentoRequestDTO documentoRequestDTO, UserDetails userDetails);
+    List<DocumentoResponseDTO> getDocumentoByClienteId(Long clienteId, UserDetails userDetails);
     void apagarDocumento(Long id);
 }
